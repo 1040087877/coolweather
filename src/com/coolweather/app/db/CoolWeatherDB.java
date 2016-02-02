81642b1c08d0c379746a0e4481de3f381b39f537
ban <1040087877@qq.com>
2016-02-03 00:54:13 +0800

    完成遍历省市县三级列表的功能

--- src/com/coolweather/app/db/CoolWeatherDB.java
+++ src/com/coolweather/app/db/CoolWeatherDB.java
@@ -105,7 +105,7 @@ public class CoolWeatherDB {
 			ContentValues values=new ContentValues();
 			values.put("county_name", county.getCountyName());
 			values.put("county_code", county.getCountyCode());
-			values.put("city_id", county.getCountyCode());
+			values.put("city_id", county.getCityId());
 			db.insert("County", null, values);
 		}
 	}
@@ -114,14 +114,14 @@ public class CoolWeatherDB {
 	 */
 	public List<County>loadCounties(int cityId){
 		List<County>list=new ArrayList<County>();
-		Cursor cursor=db.query("County", null, "cityId=?", new String[]{String.valueOf(cityId)}, null, null, null);
+		Cursor cursor=db.query("County", null, "city_id=?", new String[]{String.valueOf(cityId)}, null, null, null);
 		if(cursor.moveToFirst()){
 			do{
 				County county=new County();
 				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
 				county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
 				county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
-				county.setCityId(cursor.getInt(cursor.getColumnIndex("city_id ")));
+				county.setCityId(cursor.getInt(cursor.getColumnIndex("city_id")));
 				list.add(county);
 			}while(cursor.moveToNext());
 		}
